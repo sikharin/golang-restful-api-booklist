59bd3e382b67534193625074f8e6b5505efe2ac2
Sikharin Suwannnatee <sikharin@outlook.com>
2021-05-13 18:29:25 +0700

    Create the router and register api-endpoints and handler

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sikharin/golang-restful-api-booklist
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Book struct {
 	ID     string `json:id`
 	Title  string `json:title`
@@ -10,4 +17,32 @@ type Book struct {
 var books []Book
 
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	log.Println("Gets all books")
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Gets one book")
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Adds one book")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Update a book")
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Removes a book")
 }
