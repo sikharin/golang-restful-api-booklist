9d47a31d26e14810d69ab7a98d36e8df5b5788bd
Sikharin Suwannnatee <sikharin@outlook.com>
2021-05-14 15:19:16 +0700

    Implement 'addBook' and use Postman for http requests

--- main.go
+++ main.go
@@ -52,7 +52,13 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Adds one book")
+	var book Book
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	books = append(books, book)
+
+	json.NewEncoder(w).Encode(books)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
